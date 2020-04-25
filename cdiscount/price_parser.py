18886a3a8f5af1f216c7cbe8e161b4e979cf2847
Maël Querré <mael.querre@gmail.com>
2020-04-25 18:51:39 +0200

    Add package, module and method

---
+++ cdiscount/price_parser.py
@@ -0,0 +1,25 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse_price(sku):
+    """
+    Parse a product page and return its price.
+
+    :param sku: The unique identifier for the product
+    :return: The price of the product
+    """
+    url = "https://www.cdiscount.com/f-0-" + sku + ".html"
+    main_price_class = "fpPrice price jsMainPrice jsProductPrice hideFromPro"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price_element = soup.find_all(class_=main_price_class)[0]
+
+    return price_element["content"]
+
+
+if __name__ == "__main__":
+    print(parse_price("5225b006"))
