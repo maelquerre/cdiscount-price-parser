899f6d27c844aa236bf8b628828643124cb55871
Maël Querré <mael.querre@gmail.com>
2020-04-30 18:21:10 +0200

    Use Flask

--- api/price.py
+++ api/price.py
@@ -2,32 +2,31 @@ from http.server import BaseHTTPRequestHandler
 from requests.exceptions import HTTPError
 from urllib.parse import urlparse, parse_qs
 from cdiscount.price_parser import parse_price
+from flask import Flask, request
+
+app = Flask(__name__)
 
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        query = parse_qs(urlparse(self.path).query)
-        product_sku = query["sku"][0]
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    product_sku = request.args["sku"]
 
-        if product_sku is not None:
-            try:
-                price = parse_price(product_sku)
+    if product_sku is not None:
+        try:
+            price = parse_price(product_sku)
 
-                if price is None: # The product could not be found
-                    status_code = 404
-                    response = "Product not found."
-                else:
-                    status_code = 200
-                    response = price
-            except HTTPError as exception:
-                status_code = exception.response.status_code
-                response = exception.response.text
-        else:
-            status_code = 404
-            response = "Product not found."
+            if price is None:  # The product could not be found
+                status = 404
+                response = "Product not found."
+            else:
+                status = 200
+                response = price
+        except HTTPError as exception:
+            status = exception.response.status_code
+            response = exception.response.text
+    else:
+        status = 404
+        response = "Product not found."
 
-        self.send_response(status_code)
-        self.send_header("Content-type", "text/json")
-        self.end_headers()
-        self.wfile.write(bytes(response, "utf-8"))
-        return
+    return response, status, {"Content-type": "text/json"}
